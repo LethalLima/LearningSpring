81f3159ac2689a9027bf26ba77f2fba42a027be7
Jeffrey Lima <jeffrey.lima@xypro.com>
2016-04-22 11:36:46 -0700

    Autowired annotations.

--- spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,24 +4,32 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
-public class Logger {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Logger {
+    // Autowiring the fields does not require setters.
+//    @Autowired
 	private ConsoleWriter consoleWriter;
+//    @Autowired
 	private FileWriter fileWriter;
 
-//	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter){
-//		this.consoleWriter = consoleWriter;
-//		this.fileWriter = fileWriter;
-//	}
-//
-//	public Logger(){
-//
-//	}
+    /*
+    // Autowiring the constructor
+    @Autowired
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter){
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
+    */
 
+
+	// Autowiring the setters
+	@Autowired(required = false)
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 
+	@Autowired
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
@@ -31,7 +39,8 @@ public class Logger {
 	}
 
 	public void writeConsole(String text) {
-		consoleWriter.write(text);
+        if(consoleWriter != null)
+    		consoleWriter.write(text);
 	}
 
 }
