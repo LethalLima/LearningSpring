322e1845a6e4be7d2dea86f45a74f2693614f0c5
Jeffrey Lima <jeffrey.lima@xypro.com>
2016-04-22 12:03:08 -0700

    Resource annotations.

--- spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,33 +4,20 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 
 public class Logger {
-    // Autowiring the fields does not require setters.
-//    @Autowired
+
 	private ConsoleWriter consoleWriter;
-//    @Autowired
-	private FileWriter fileWriter;
+	private LogWriter fileWriter;
 
-    /*
-    // Autowiring the constructor
-    @Autowired
-	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter){
-		this.consoleWriter = consoleWriter;
-		this.fileWriter = fileWriter;
-	}
-    */
-
-
-	// Autowiring the setters
-	@Autowired(required = false)
+    @Resource
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 
-	@Autowired
-	public void setFileWriter(FileWriter fileWriter) {
+    @Resource(name = "squirrel")
+	public void setFileWriter(LogWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
 
