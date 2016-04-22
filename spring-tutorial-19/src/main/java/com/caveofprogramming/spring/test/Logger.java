ecd7f7bf46bcec72267e9b2caa3c7e59a5f3c8e8
Jeffrey Lima <jeffrey.lima@xypro.com>
2016-04-22 13:00:02 -0700

    Automatic Bean Discovery.

--- spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ spring-tutorial-19/src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,27 +4,29 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
+import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+@Component
 public class Logger {
 
 	private ConsoleWriter consoleWriter;
 	private LogWriter fileWriter;
 
-//    @Resource
     @Inject
-    @Named(value = "consoleWriter")
+    // or @Autowired
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 
-//    @Resource(name = "squirrel")
     @Inject
-    @Named(value = "squirrel")
+    // or @Autowired
+    @Named(value = "fileWriter")
 	public void setFileWriter(LogWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
